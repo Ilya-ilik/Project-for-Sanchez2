4b4328ec714964a249be045626f7cabdc25a9923
Ilya-ilik <101934601+Ilya-ilik@users.noreply.github.com>
2023-01-29 17:46:55 +0300

    +front commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,78 @@
 // import logo from './logo.svg';
+import Vector from './images/Vector.png';
+
 import "./App.css";
 import axios, { Axios } from "axios";
 import React, { useEffect, useState, useRef } from "react";
 
-function App() {
+function App(props) {
+
+  let config2 = "http://bibapass.ru/api/v1/passwords";
+
+  console.log(props.config)
+
   const [table, setTable] = useState([]);
   //Получение данных с бека
   useEffect(() => {
-    axios.get("http://bibapass.ru/api/v1/passwords").then((json) => {
+    axios.get(props.config).then((json) => {
       setTable(json.data.password);
     });
   }, []);
 
   let refreshPage = () => {
-    window.location.reload();
+    // window.location.reload();
+
+    axios.get(props.config).then((json) => {
+      setTable(json.data.password);
+    });
   };
 
   const textAreaRef2 = useRef(null);
 
   let copyTextInput = () => {
-    textAreaRef2.current.select(); // Эта строка выбирает от куда копировать
+    textAreaRef2.current.select(); // Эта строка выделяет что будет скопировано от куда копировать
     document.execCommand("copy"); // Бех этой строки не копируется
   };
 
+
+  // let testFunction = () => { 
+  //   console.log("da")
+  // };
+
+  
+
   return (
     <div className="App">
-      <input value={table} ref={textAreaRef2}></input>
 
-      <button type="button" onClick={refreshPage}>
+{/* код ползунка */}
+
+      
+{/* код ползунка */}
+
+
+      <div className="central_block">
+        <div className="logo">bibapass</div>
+        <p className="first_text">простой генератор паролей</p>
+        <div className="input" ><div className='input_password' ref={textAreaRef2} >{table}</div> <img src={Vector} onClick={copyTextInput} /></div> 
+        {/* <div className='button'><p className='button_text'>Сгенерировать пароль</p></div> */}
+        <div className='button' onClick={refreshPage}>Сгенерировать пароль</div>
+        <p className='difficulty_text'>Нужен сложный?</p>
+        <input className='polzunok' type='range' min={1} max={800}step={1}></input> {/* это строка с ползунком */}
+        <p className='copyright'>Bibaboys © 2023</p>
+      </div>
+     
+
+
+
+      {/* <input className="input" value={table} ref={textAreaRef2} ></input>  */}
+
+      {/* <input value={table} ref={textAreaRef2}></input> */}
+
+      {/* <button type="button" onClick={refreshPage}>
         Новый пароль
-      </button>
+      </button> */}
 
-      <button onClick={copyTextInput}>Копировать пароль</button>
+      {/* <button onClick={copyTextInput}>Копировать пароль</button>  */}
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,4 @@
+let config = "http://bibapass.ru/api/v1/passwords";
+export default config;
+
+// http://bibapass.ru/api/v1/passwords APi
